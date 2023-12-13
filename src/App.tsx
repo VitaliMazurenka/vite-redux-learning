b0861c1efef5770f95dafad7436b29bed1f75636
Vitali Mazurenka <vitali.mazurenka@web.de>
2023-12-13 19:26:35 +0100

    Add components and assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import GlobalStyles from "./styles/GlobalStyles";
+
 function App() {
   return (
-    <div>Redux Project</div>
+    <div>
+      <GlobalStyles />
+      Redux project
+    </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,8 @@ import { Provider } from "react-redux"
 import { store } from "./store/store"
 import App from "./App"
 
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  
-    <Provider store={store}>
-      <App />
-    </Provider>
-  
+  <Provider store={store}>
+    <App />
+  </Provider>,
 )
